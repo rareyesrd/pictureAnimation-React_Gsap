b9084b10dfaf85ab0b2bbb4aa797e0d6ec5ddbc0
Rafael Reyes <rafaelalexanderr5@gmail.com>
2020-01-14 02:33:40 -0400

    COMPLETE - Image animation using gsap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect} from 'react';
+import cat from './cat-image.jpg';
 import './App.css';
+import CSSRulePlugin from 'gsap/CSSRulePlugin';
+import { Power2, TimelineLite } from 'gsap';
 
 function App() {
+
+  let container = useRef(null)
+  let image = useRef(null)
+  let imageReveal = CSSRulePlugin.getRule('.image-container:after');
+
+  const tl = new TimelineLite();
+  useEffect(() => {
+    tl.to(container, 1, {css: {visibility: 'visible'}})
+    .to(imageReveal,
+      1.4,
+      {width:'0%', ease: Power2.easeInOut})
+      .from(image, 1.4, {scale: 1.6, ease: Power2.easeInOut, delay: -1.6 })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div ref={el => { container = el}} className="container">
+        <>
+          <div className="image-container">
+            <img ref={el => { image = el}} src={cat} alt="Cat"/>
+          </div>
+        </>
+      </div>
     </div>
   );
 }
