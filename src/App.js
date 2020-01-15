7b67818cffadf8071daf1e6bb57bef0aa90cb49e
Rafael Reyes <r.reyes@caribemedia.com.do>
2020-01-15 12:39:30 -0400

    ADD - title animation

--- src/App.js
+++ src/App.js
@@ -2,30 +2,28 @@ import React, {useRef, useEffect} from 'react';
 import cat from './cat-image.jpg';
 import './App.css';
 import CSSRulePlugin from 'gsap/CSSRulePlugin';
-import { Power2, TimelineLite } from 'gsap';
+import { TweenMax, Power2, TimelineLite } from 'gsap';
 
 function App() {
-
+  let title = useRef(null)
   let container = useRef(null)
   let image = useRef(null)
   let imageReveal = CSSRulePlugin.getRule('.image-container:after');
 
   const tl = new TimelineLite();
   useEffect(() => {
-    tl.to(container, 1, {css: {visibility: 'visible'}})
-    .to(imageReveal,
-      1.4,
-      {width:'0%', ease: Power2.easeInOut})
-      .from(image, 1.4, {scale: 1.6, ease: Power2.easeInOut, delay: -1.6 })
-  })
+    tl.to(container, 1.4, {css: {visibility: 'visible'}})
+    .to(imageReveal, 1.4, {width:'0%', ease: Power2.easeInOut})
+    .from(image, 1.8, {scale: 1.6, ease: Power2.easeInOut, delay: -2 })
+    TweenMax.to( title, 1, {opacity: '1', y: -5, ease: Power2.easeInOut, delay: 1}, 1.2 )
+  });
   return (
     <div className="main">
-      <div ref={el => { container = el}} className="container">
-        <>
+      <div ref={el => { container = el }} className="container">
+      <h1 ref={el => { title = el }} className="title">Fancy image reveal</h1>
           <div className="image-container">
-            <img ref={el => { image = el}} src={cat} alt="Cat"/>
+            <img ref={el => { image = el }} src={cat} alt="Cat"/>
           </div>
-        </>
       </div>
     </div>
   );
